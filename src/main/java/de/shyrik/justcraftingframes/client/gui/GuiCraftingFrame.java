ec54f89d67fe8c3516efe439c6706c774c23a120
Shyrik <r.wesenberg@gmail.com>
2018-08-03 13:52:49 +0200

    gui still not functional

--- src/main/java/de/shyrik/justcraftingframes/client/gui/GuiCraftingFrame.java
+++ src/main/java/de/shyrik/justcraftingframes/client/gui/GuiCraftingFrame.java
@@ -1,13 +1,17 @@
 package de.shyrik.justcraftingframes.client.gui;
 
+import com.teamwizardry.librarianlib.features.container.InventoryWrapper;
 import com.teamwizardry.librarianlib.features.container.builtin.BaseWrappers;
 import com.teamwizardry.librarianlib.features.gui.GuiBase;
 import com.teamwizardry.librarianlib.features.gui.components.ComponentSprite;
+import com.teamwizardry.librarianlib.features.gui.components.ComponentVoid;
+import com.teamwizardry.librarianlib.features.guicontainer.ComponentSlot;
 import com.teamwizardry.librarianlib.features.guicontainer.builtin.BaseLayouts;
 import com.teamwizardry.librarianlib.features.sprite.Sprite;
 import de.shyrik.justcraftingframes.JustCraftingFrames;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.items.wrapper.InvWrapper;
 
 public class GuiCraftingFrame extends GuiBase {
 
@@ -17,15 +21,29 @@ public class GuiCraftingFrame extends GuiBase {
     private EntityPlayer player;
 
     public GuiCraftingFrame(EntityPlayer player) {
-        super(250, 224);
+        super(175, 165);
         this.player = player;
 
-        tableComponent = new ComponentSprite(FRAME_SPRITE, 0, 0, 250, 224);
+        tableComponent = new ComponentSprite(FRAME_SPRITE, 0, 0, 175, 165);
         getMainComponents().add(tableComponent);
 
-        BaseWrappers.InventoryWrapperPlayer iwp = BaseWrappers.INSTANCE.player(player);
-        BaseLayouts.PlayerLayout pl = new BaseLayouts.PlayerLayout(iwp);
 
-        getMainComponents().add(pl.getMain());
+        ComponentVoid pInventWrap = new ComponentVoid(7,83);
+        BaseWrappers.InventoryWrapperPlayer iwp = BaseWrappers.INSTANCE.player(player);
+        ComponentVoid pInvent = new ComponentVoid(0, 0);
+        for ( int row = 0; row < 2; row++) {
+            for ( int col = 0; col < 8; col++) {
+                pInvent.add(new ComponentSlot(iwp.getMain().get(row * 9 + col), col * 18, row * 18));
+            }
+        }
+        ComponentVoid hotbar = new ComponentVoid(0, 58);
+        for ( int col = 0; col < 8; col++) {
+            hotbar.add(new ComponentSlot(iwp.getHotbar().get(col), col * 18, 0));
+        }
+        pInvent.add(hotbar);
+        pInvent.clipping.setClipToBounds(true);
+        pInventWrap.add(pInvent);
+
+        getMainComponents().add(pInventWrap);
     }
 }
--- src/main/java/de/shyrik/justcraftingframes/common/CommonProxy.java
+++ src/main/java/de/shyrik/justcraftingframes/common/CommonProxy.java
@@ -1,14 +1,19 @@
 package de.shyrik.justcraftingframes.common;
 
+import de.shyrik.justcraftingframes.JustCraftingFrames;
+import de.shyrik.justcraftingframes.client.gui.GuiHandler;
 import de.shyrik.justcraftingframes.init.Blocks;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent event){
         Blocks.init();
+
+        NetworkRegistry.INSTANCE.registerGuiHandler(JustCraftingFrames.instance, new GuiHandler());
     }
 
     public void init(FMLInitializationEvent event) {
