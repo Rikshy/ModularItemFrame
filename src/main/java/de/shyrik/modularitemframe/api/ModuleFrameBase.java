5c44a1272539dca219344005a6cc934dacaac553
Shyrik <r.wesenberg@gmail.com>
2018-08-13 13:57:38 +0200

    documentated api classes
    module dropping is now part of the module not the driver or block

--- src/main/java/de/shyrik/modularitemframe/api/ModuleFrameBase.java
+++ src/main/java/de/shyrik/modularitemframe/api/ModuleFrameBase.java
@@ -1,7 +1,8 @@
 package de.shyrik.modularitemframe.api;
 
+import de.shyrik.modularitemframe.ModularItemFrame;
+import de.shyrik.modularitemframe.api.utils.ItemUtils;
 import de.shyrik.modularitemframe.client.render.FrameRenderer;
-import de.shyrik.modularitemframe.common.container.ContainerCraftingFrame;
 import de.shyrik.modularitemframe.common.tile.TileModularFrame;
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
@@ -14,6 +15,8 @@ import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -39,19 +42,45 @@ public abstract class ModuleFrameBase implements INBTSerializable<NBTTagCompound
 		tile = te;
 	}
 
+	/**
+	 * Is called when the {@link FrameRenderer} wants to render the module for the first time.
+	 * @see #bakeModel(IModel)
+	 *
+	 * @return [Nonnull] {@link ResourceLocation} to the Texture
+	 */
 	@Nonnull
 	public abstract ResourceLocation frontTexture();
 
+	/**
+	 * Is called when the {@link FrameRenderer} wants to render the module for the first time.
+	 * @see #bakeModel(IModel)
+	 *
+	 * @return [Nonnull] {@link ResourceLocation} to the Texture
+	 */
 	@Nonnull
 	public ResourceLocation backTexture() {
 		return new ResourceLocation("minecraft", "blocks/log_birch_top");
 	}
 
+	/**
+	 * TOP and WAILA are using this for display
+	 * Please use translation holders - raw strings are bad!
+	 *
+	 * @return the name of the module :O
+	 */
 	public abstract String getModuleName();
 
 	public boolean reloadModel = false;
 	private IBakedModel bakedModel = null;
 
+	/**
+	 * Called by the {@link FrameRenderer} to bake the Frame model
+	 * by default {@link #frontTexture()} and {@link #backTexture()} will be asked to be replaced
+	 * override this with caution.
+	 *
+	 * @param model Contains the model of the frame
+	 * @return baked model ofc
+	 */
 	public IBakedModel bakeModel(IModel model) {
 		if (bakedModel == null || reloadModel) {
 			bakedModel = model.bake(model.getDefaultState(), DefaultVertexFormats.ITEM, location -> {
@@ -66,38 +95,77 @@ public abstract class ModuleFrameBase implements INBTSerializable<NBTTagCompound
 		return bakedModel;
 	}
 
+	/**
+	 * Called by the {@link FrameRenderer} after rendering the frame.
+	 * Extra rendering can be don here
+	 * like the {@link de.shyrik.modularitemframe.common.module.ModuleItem ModuleItem} does the item thing)
+	 *
+	 * @param tesr instance of the current {@link FrameRenderer}
+	 */
 	public void specialRendering(FrameRenderer tesr, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 	}
 
+	/**
+	 * Called when the frame got left clicked
+	 */
 	public void onBlockClicked(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull EntityPlayer playerIn) {
 	}
 
-	public boolean hasScrewInteraction() {
-		return true;
-	}
-
+	/**
+	 * Called when a {@link de.shyrik.modularitemframe.common.item.ItemScrewdriver screwdriver} in interaction mode clicks a frame
+	 * Implement behavior for {@link de.shyrik.modularitemframe.common.item.ItemScrewdriver screwdriver} interaction here
+	 *
+	 * @param driver the driver who was used
+	 */
 	public void screw(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull EntityPlayer playerIn, ItemStack driver) {
 
 	}
 
+	/**
+	 * Called when a frame is simply right clicked
+	 */
 	public abstract boolean onBlockActivated(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull EntityPlayer playerIn, @Nonnull EnumHand hand, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ);
 
-	public ContainerCraftingFrame createContainer(final EntityPlayer player) {
+	/**
+	 * in case your module has a gui
+	 */
+	public Container createContainer(final EntityPlayer player) {
 		return null;
 	}
 
+	/**
+	 * called when the tile entity ticks
+	 */
 	public void tick(@Nonnull World world, @Nonnull BlockPos pos) {
 	}
 
+	/**
+	 * Called when module is removed with the {@link de.shyrik.modularitemframe.common.item.ItemScrewdriver screwdriver}
+	 * or destroyed.
+	 * If you want the module to drop, make sure to call the super method
+	 */
 	public void onRemove(@NotNull World worldIn, @NotNull BlockPos pos, @Nullable EntityPlayer playerIn) {
-
+		Item item = Item.getByNameOrId(ModularItemFrame.MOD_ID + ":" + ModuleRegistry.getModuleId(tile.module.getClass()));
+		if (item instanceof ItemModule) {
+			ItemStack remain = new ItemStack(item);
+			if (playerIn != null)
+				remain = ItemUtils.giveStack(ItemUtils.getPlayerInv(playerIn), remain);
+			if (!remain.isEmpty())
+				ItemUtils.ejectStack(worldIn, tile.getPos(), tile.blockFacing(), remain);
+		}
 	}
 
+	/**
+	 * The One Probe information handling
+	 */
 	@Optional.Method(modid = "theoneprobe")
 	public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
 		probeInfo.horizontal().text(I18n.format("modularitemframe.tooltip.module", getModuleName()));
 	}
 
+	/**
+	 * Waila/Hwyla information handling
+	 */
 	@Nonnull
 	@Optional.Method(modid = "waila")
 	public List<String> getWailaBody(ItemStack itemStack, IWailaDataAccessor accessor, IWailaConfigHandler config) {
@@ -106,11 +174,18 @@ public abstract class ModuleFrameBase implements INBTSerializable<NBTTagCompound
 		return tips;
 	}
 
+	/**
+	 * NBT serialization in case there are some data to be saved!
+	 * this gets synced automatically
+	 */
 	@Override
 	public NBTTagCompound serializeNBT() {
 		return new NBTTagCompound();
 	}
 
+	/**
+	 * NBT deserialization in case there are some data to be saved!
+	 */
 	@Override
 	public void deserializeNBT(NBTTagCompound nbtTagCompound) {
 
--- src/main/java/de/shyrik/modularitemframe/api/ModuleRegistry.java
+++ src/main/java/de/shyrik/modularitemframe/api/ModuleRegistry.java
@@ -1,6 +1,5 @@
 package de.shyrik.modularitemframe.api;
 
-import de.shyrik.modularitemframe.common.item.ItemModule;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -10,15 +9,35 @@ public class ModuleRegistry {
 
 	private static Map<String, Class<? extends ModuleFrameBase>> modules = new HashMap<>();
 
+	/**
+	 * registers a module class
+	 * @param id unique id
+	 * @param moduleClass module to register
+	 *
+	 * @throws IllegalArgumentException when id is duplicated
+	 */
 	public static void register(String id, Class<? extends ModuleFrameBase> moduleClass) {
+		if(modules.containsKey(id))
+			throw new IllegalArgumentException("[ModularItemFrame] module key already exists!");
 		modules.put(id, moduleClass);
 	}
 
+	/**
+	 * Registers the module class and creates a new {@link ItemModule} instance
+	 * @param id unique id
+	 * @param moduleClass module to register
+	 * @return new {@link ItemModule} instance
+	 */
 	public static ItemModule registerCreate(String id, Class<? extends ModuleFrameBase> moduleClass) {
 		register(id, moduleClass);
 		return new ItemModule(id);
 	}
 
+	/**
+	 * creates an instance of a module
+	 * @param id the id of the module
+	 * @return created instance
+	 */
 	@Nullable
 	public static ModuleFrameBase createModuleInstance(String id) {
 		try {
@@ -29,6 +48,11 @@ public class ModuleRegistry {
 		return null;
 	}
 
+	/**
+	 * Gets the module id of a specific module
+	 * @param moduleClass module to look up
+	 * @return unique module id
+	 */
 	public static String getModuleId(Class<? extends ModuleFrameBase> moduleClass) {
 		for (Map.Entry<String, Class<? extends ModuleFrameBase>> entry : modules.entrySet())
 			if (entry.getValue() == moduleClass) return entry.getKey();
