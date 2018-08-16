df9e14ceba009405cdf17bd53e58336fe3b334c5
Shyrik <r.wesenberg@gmail.com>
2018-08-16 22:16:17 +0200

    read last commit

--- src/main/java/de/shyrik/modularitemframe/common/module/t2/ModuleXP.java
+++ src/main/java/de/shyrik/modularitemframe/common/module/t2/ModuleXP.java
@@ -1,6 +1,5 @@
-package de.shyrik.modularitemframe.common.module.t1;
+package de.shyrik.modularitemframe.common.module.t2;
 
-import de.shyrik.modularitemframe.api.ConfigValues;
 import de.shyrik.modularitemframe.api.utils.XpUtils;
 import de.shyrik.modularitemframe.common.module.t1.ModuleItem;
 import mcjty.theoneprobe.api.IProbeHitData;
@@ -17,9 +16,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.fml.common.Optional;
@@ -32,14 +29,13 @@ import java.util.List;
 //thx openblocks and enderio
 public class ModuleXP extends ModuleItem {
 
-    private static final int MAX_XP = 21862;
+    protected static final int MAX_XP = 21862;
 
     private static final String NBT_XP = "xp";
     private static final String NBT_LEVEL = "level";
 
     public int experience;
     public int levels;
-    public EnumMode mode = EnumMode.IN;
 
     public ModuleXP() {
         displayItem = new ItemStack(Items.EXPERIENCE_BOTTLE);
@@ -51,12 +47,11 @@ public class ModuleXP extends ModuleItem {
     }
 
     @Override
-    public void screw(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull EntityPlayer playerIn, ItemStack driver) {
-        if (!world.isRemote) {
-            int modeIdx = mode.getIndex() + 1;
-            if (modeIdx == EnumMode.values().length) modeIdx = 0;
-            mode = EnumMode.VALUES[modeIdx];
-            playerIn.sendMessage(new TextComponentTranslation("modularitemframe.message.xp_mode_change", mode.getName()));
+    public void onBlockClicked(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull EntityPlayer playerIn) {
+        if (!worldIn.isRemote) {
+            if (playerIn.isSneaking()) drainContainerXpToReachPlayerLevel(playerIn, 0);
+            else drainContainerXpToReachPlayerLevel(playerIn, playerIn.experienceLevel + 1);
+            tile.markDirty();
         }
     }
 
@@ -65,29 +60,11 @@ public class ModuleXP extends ModuleItem {
         if (playerIn instanceof FakePlayer) return false;
 
         if (!worldIn.isRemote) {
-            if (mode == EnumMode.IN) {
-                if (playerIn.isSneaking()) drainPlayerXpToReachPlayerLevel(playerIn, 0);
-                else drainPlayerXpToReachPlayerLevel(playerIn, playerIn.experienceLevel - 1);
-            } else {
-                if (playerIn.isSneaking()) drainContainerXpToReachPlayerLevel(playerIn, 0);
-                else drainContainerXpToReachPlayerLevel(playerIn, playerIn.experienceLevel + 1);
-            }
+            if (playerIn.isSneaking()) drainPlayerXpToReachPlayerLevel(playerIn, 0);
+            else drainPlayerXpToReachPlayerLevel(playerIn, playerIn.experienceLevel - 1);
             tile.markDirty();
         }
         return true;
-    }
-
-    @Override
-    public void tick(@Nonnull World world, @Nonnull BlockPos pos) {
-        if (mode != EnumMode.VACUUM || experience >= MAX_XP) return;
-        if (world.getTotalWorldTime() % ConfigValues.VacuumCooldown != 0) return;
-
-        List<EntityXPOrb> entities = world.getEntitiesWithinAABB(EntityXPOrb.class, getVacuumBB(pos));
-        for (EntityXPOrb entity : entities) {
-            if (entity.isDead) continue;
-
-            addExperience(entity.getXpValue());
-        }
     }
 
     private void drainPlayerXpToReachPlayerLevel(@Nonnull EntityPlayer player, int level) {
@@ -102,7 +79,7 @@ public class ModuleXP extends ModuleItem {
         }
     }
 
-    private int addExperience(int xpToAdd) {
+    protected int addExperience(int xpToAdd) {
         int j = MAX_XP - experience;
         if (xpToAdd > j) {
             xpToAdd = j;
@@ -123,28 +100,9 @@ public class ModuleXP extends ModuleItem {
         XpUtils.addPlayerXP(player, requiredXP);
     }
 
-    private AxisAlignedBB getVacuumBB(@Nonnull BlockPos pos) {
-        int range = ConfigValues.MaxVacuumRange;
-        switch (tile.blockFacing()) {
-            case DOWN:
-                return new AxisAlignedBB(pos.add(-range, 0, -range), pos.add(range, range, range));
-            case UP:
-                return new AxisAlignedBB(pos.add(-range, 0, -range), pos.add(range, -range, range));
-            case NORTH:
-                return new AxisAlignedBB(pos.add(-range, -range, 0), pos.add(range, range, range));
-            case SOUTH:
-                return new AxisAlignedBB(pos.add(-range, -range, 0), pos.add(range, range, -range));
-            case WEST:
-                return new AxisAlignedBB(pos.add(0, -range, -range), pos.add(-range, range, range));
-            case EAST:
-                return new AxisAlignedBB(pos.add(0, -range, -range), pos.add(range, range, range));
-        }
-        return new AxisAlignedBB(pos, pos.add(1, 1, 1));
-    }
-
     @Override
-    public void onRemove(@NotNull World worldIn, @NotNull BlockPos pos, @Nullable EntityPlayer playerIn) {
-        super.onRemove(worldIn, pos, playerIn);
+    public void onRemove(@NotNull World worldIn, @NotNull BlockPos pos, @Nonnull EnumFacing facing, @Nullable EntityPlayer playerIn) {
+        super.onRemove(worldIn, pos, facing, playerIn);
         if (playerIn == null || playerIn instanceof FakePlayer)
             worldIn.spawnEntity(new EntityXPOrb(worldIn, pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, experience));
         else playerIn.addExperience(experience);
@@ -178,32 +136,5 @@ public class ModuleXP extends ModuleItem {
         super.deserializeNBT(nbt);
         if (nbt.hasKey(NBT_XP)) experience = nbt.getInteger(NBT_XP);
         if (nbt.hasKey(NBT_LEVEL)) levels = nbt.getInteger(NBT_LEVEL);
-    }
-
-    public enum EnumMode {
-        IN(0, "modularitemframe.message.xp_mode_change.in"), OUT(1, "modularitemframe.message.xp_mode_change.out"), VACUUM(2, "modularitemframe.message.xp_mode_change.vacuum");
-
-        public static final EnumMode[] VALUES = new EnumMode[3];
-
-        private final int index;
-        private final String name;
-
-        EnumMode(int indexIn, String nameIn) {
-            index = indexIn;
-            name = nameIn;
-        }
-
-        public int getIndex() {
-            return this.index;
-        }
-
-        public String getName() {
-            return I18n.format(this.name);
-        }
-
-        static {
-            for (EnumMode enummode : values())
-                VALUES[enummode.index] = enummode;
-        }
     }
 }
