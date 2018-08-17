1153cd9df2e62374db79267bea7756a5532b2dee
Shyrik <r.wesenberg@gmail.com>
2018-08-17 18:31:20 +0200

    not used anymore since the last commit

--- src/main/java/de/shyrik/modularitemframe/api/ConfigValues.java
+++ src/main/java/de/shyrik/modularitemframe/api/ConfigValues.java
@@ -5,9 +5,6 @@ import com.teamwizardry.librarianlib.features.config.ConfigProperty;
 
 public final class ConfigValues {
 
-	@ConfigProperty(category = "general", comment = "When the crafting frame is put on a chest (or other inventory) it will also use player inventory to craft")
-	public static boolean StillUsePlayerInv = true;
-
 	@ConfigProperty(category = "general", comment = "Allow fake players to interact with frames")
 	public static boolean AllowFakePlayers = false;
 
